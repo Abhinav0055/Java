4d716dec4b3a12320cd4890617d2f0014353b50b
Abhinav0055 <abhinav0055.be21@chitkara.edu.in>
2022-06-18 20:15:58 +0530

    practice

---
+++ RectanglePatterns.java
@@ -0,0 +1,14 @@
+public class RectanglePatterns {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=y;j++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+    
+}
